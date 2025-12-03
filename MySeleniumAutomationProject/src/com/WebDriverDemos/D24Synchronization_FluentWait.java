5d2f59d2c61a87017ee031414223bf51370ed3fa
Ankush <ankushvankore@gmail.com>
2025-12-03 19:52:50 +0530

    Date: 03 Dec 2025

---
+++ MySeleniumAutomationProject/src/com/WebDriverDemos/D24Synchronization_FluentWait.java
@@ -0,0 +1,56 @@
+package com.WebDriverDemos;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+
+public class D24Synchronization_FluentWait {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.redbus.in/");
+
+		FluentWait<WebDriver>wait = new FluentWait<WebDriver>(driver);
+		
+		//From
+		driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/search/div/div/div/div[1]/div[1]/div[1]/div[1]/div/div[1]")).click();
+		/*wait.withTimeout(Duration.ofSeconds(10))
+		.ignoring(NoSuchElementException.class)
+		.pollingEvery(Duration.ofMillis(1))
+		.until(ExpectedConditions.visibilityOfElementLocated(By.id("scrDest"))).sendKeys("Kolh");*/
+		Thread.sleep(2000);
+		driver.findElement(By.id("srcDest")).sendKeys("Kolh");
+		wait.withTimeout(Duration.ofSeconds(10))
+		.ignoring(NoSuchElementException.class)
+		.pollingEvery(Duration.ofMillis(1))
+		.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/main/div/div/search/div/div/div/div[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div/div/div"))).click();
+		//driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/search/div/div/div/div[1]/div[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div/div/div")).click();
+
+		//To
+		//driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/search/div/div/div/div[1]/div[2]/div/div/div[1]")).click();
+		driver.findElement(By.id("srcDest")).sendKeys("Ban");
+		driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/search/div/div/div/div[1]/div[2]/div[2]/div[4]/div[1]/div[1]/div/div/div")).click();
+
+		//Calendar
+		driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/search/div/div/div/div[1]/div[3]/div[1]/i")).click();
+
+		//Date
+		driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/search/div/div/div/div[1]/div[3]/div[3]/div[2]/div/ul/li[31]/div/div/span")).click();
+
+		//Search Bus
+		driver.findElement(By.xpath("//*[@id=\"root\"]/main/div/div/search/div/div/button")).click();
+
+		//Display Result Bus
+		System.out.println(driver.findElement(By.xpath("//*[@id=\"42628206\"]/div[2]/div[3]/div/div[1]/div[1]")).getText());
+		
+		driver.close();
+	}
+
+}
