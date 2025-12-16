1af4314aad206083dd6f8146b9b2841415f8d8eb
Ankush <ankushvankore@gmail.com>
2025-12-16 08:59:19 +0530

    Date: 16 Dec 2025

--- MySeleniumAutomationProject/src/com/TestNGDemos/D05GoogleAssignment.java
+++ MySeleniumAutomationProject/src/com/TestNGDemos/D05GoogleAssignment.java
@@ -1,60 +1,59 @@
 package com.TestNGDemos;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
-public class D05AjioAssignment {
+public class D05GoogleAssignment {
 	WebDriver driver;
 	@Test(priority = 1)
-	public void clickOnMenLink() {
-		driver.findElement(By.linkText("MEN")).click();
+	public void clickOnGmail() {
+		driver.findElement(By.linkText("Gmail")).click();
 	}
 	
 	@Test(priority = 2)
-	public void clickOnWomenLink() {
-		driver.findElement(By.linkText("WOMEN")).click();
+	public void clickOnImages() {
+		driver.findElement(By.linkText("Images")).click();
 	}
 	
 	@Test(priority = 3)
-	public void clickOnKidsLink() {
-		driver.findElement(By.linkText("KIDS")).click();
+	public void clickOnAdvt() {
+		driver.findElement(By.partialLinkText("Adve")).click();
 	}
 	
-	@Test(priority = 4, enabled = false)
-	public void clickOnBeautyLink() {
-		driver.findElement(By.linkText("BEAUTY")).click();
+	@Test(priority = 4)
+	public void clickOnAbout() {
+		driver.findElement(By.partialLinkText("About")).click();
 	}
 
-	@BeforeMethod
+	@BeforeMethod 
 	public void beforeMethod() {
-		driver.get("https://www.ajio.com/");
+		driver.get("https://google.com");
 		System.out.println("Title: " + driver.getTitle());
 	}
 
-	@AfterMethod
+	@AfterMethod 
 	public void afterMethod() {
 		System.out.println("Title: " + driver.getTitle());
 	}
 
 	@BeforeTest
 	public void beforeTest() {
-		driver = new ChromeDriver();
+		driver = new EdgeDriver();	
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
 	}
 
 	@AfterTest
 	public void afterTest() {
 		driver.close();
 	}
-
 }
--- MySeleniumAutomationProject/src/com/TestNGDemos/D07AssertionDemo.java
+++ MySeleniumAutomationProject/src/com/TestNGDemos/D07AssertionDemo.java
@@ -15,14 +15,20 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class D06DataProviderDemo {
+public class D07AssertionDemo {
 	WebDriver driver;
+	String expUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index", actUrl;
 	
 	@Test(dataProvider = "getLoginDetails")
 	public void loginToOHRM(String un, String ps) {
 		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys(un);
 		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(ps);
 		driver.findElement(By.xpath("//button[@type='submit']")).submit();
+		
+		actUrl = driver.getCurrentUrl();
+		//Assert.assertEquals(actUrl, expUrl);
+		//Assert.assertTrue(actUrl.equals(expUrl));
+		Assert.assertTrue(actUrl.contains("dashboard"));
 	}
 
 	@DataProvider
@@ -67,5 +73,4 @@ public class D06DataProviderDemo {
 	public void afterTest() {
 		driver.close();
 	}
-
 }
