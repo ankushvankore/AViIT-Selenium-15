e88e17dc5345f4630b2ed8544137a195385ba8b5
Ankush <ankushvankore@gmail.com>
2025-12-24 19:22:15 +0530

    Date: 24 Dec 2025

--- MySeleniumAutomationProject/src/com/TestNGDemos/D10POM_Client.java
+++ MySeleniumAutomationProject/src/com/TestNGDemos/D10POM_Client.java
@@ -1,6 +1,9 @@
 package com.TestNGDemos;
 
 import org.testng.annotations.Test;
+
+import graphql.Assert;
+
 import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
@@ -8,17 +11,35 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 
 public class D10POM_Client {
 	RediffUtility r1;
 	WebDriver driver;
 	
 	@Test
-	public void createUser() {
+	public void createUser() throws InterruptedException {
 		r1.setFullName("Priyanka Sutar");
 		r1.setRediffId("piyu123");
 		r1.setPassword("priyanka!234");
 		r1.setBirthDate("05", "MAR", "2000");
+		String msg = r1.checkAvailablity();
+		Assert.assertTrue(msg.contains("available"));
+	}
+	
+	@Test
+	public void createUser2() throws InterruptedException {
+		r1.setFullName("Snehasmita Senapati");
+		r1.setRediffId("snehasmita5dec");
+		r1.setPassword("sneha123");
+		r1.setBirthDate("05", "DEC", "2000");
+		String msg = r1.checkAvailablity();
+		Assert.assertTrue(msg.contains("available"));
+	}
+	@BeforeMethod
+	public void refreshPage() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.navigate().refresh();
 	}
 
 	@BeforeTest
@@ -34,6 +55,7 @@ public class D10POM_Client {
 
 	@AfterTest
 	public void afterTest() {
+		//driver.close();
 	}
 
 }
--- MySeleniumAutomationProject/src/com/TestNGDemos/RediffUtility.java
+++ MySeleniumAutomationProject/src/com/TestNGDemos/RediffUtility.java
@@ -2,21 +2,35 @@ package com.TestNGDemos;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class RediffUtility {
 	WebDriver driver;
 	
+	@FindBy(xpath = "//input[@placeholder='Enter your full name']")WebElement fullName;
+	@FindBy(xpath = "//input[starts-with(@name, \"login\")]")WebElement rediffId;
+	@FindBy(className = "day")WebElement dayDrp;
+	@FindBy(className = "middle")WebElement monthDrp;
+	@FindBy(className = "year")WebElement yearDrp;
+	@FindBy(xpath = "/html[1]/body[1]/div[2]/div[2]/form[1]/div[1]/div[4]/input[1]")WebElement btnCheck;
+	@FindBy(xpath = "//*[@id=\"check_availability\"]/div")WebElement infoMessage;
+	
 	public RediffUtility(WebDriver d) {
 		driver = d;
+		PageFactory.initElements(driver, this); 		//Will initialize all the WebElements declared above
 	}
 	
 	public void setFullName(String fn) {
-		driver.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys(fn);
+		//driver.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys(fn);
+		fullName.sendKeys(fn);
 	}
 	
 	public void setRediffId(String rid) {
-		driver.findElement(By.xpath("//input[starts-with(@name, \"login\")]")).sendKeys(rid);
+		//driver.findElement(By.xpath("//input[starts-with(@name, \"login\")]")).sendKeys(rid);
+		rediffId.sendKeys(rid);
 	}
 	
 	public void setPassword(String ps) {
@@ -25,10 +39,21 @@ public class RediffUtility {
 	}
 	
 	public void setBirthDate(String day, String month, String year) {
-		Select dayDD = new Select(driver.findElement(By.className("day")));
+		//Select dayDD = new Select(driver.findElement(By.className("day")));
+		Select dayDD = new Select(dayDrp);
 		dayDD.selectByVisibleText(day);
 		
-		new Select(driver.findElement(By.className("middle"))).selectByVisibleText(month);
-		new Select(driver.findElement(By.className("year"))).selectByVisibleText(year);
+		//new Select(driver.findElement(By.className("middle"))).selectByVisibleText(month);
+		new Select(monthDrp).selectByVisibleText(month);
+		//new Select(driver.findElement(By.className("year"))).selectByVisibleText(year);
+		new Select(yearDrp).selectByVisibleText(year);
+	}
+	
+	public String checkAvailablity() throws InterruptedException {
+		btnCheck.click();
+		Thread.sleep(2000);
+		//System.out.println(infoMessage.getText());
+		return infoMessage.getText();
+				
 	}
 }
